c039796e4d04cdc32eeea58f43203d0c96955ebb
Hackr <Hello@Camerons-MacBook-Pro.local>
2018-09-03 18:12:51 -0600

    "commit 9.0"

--- Package.swift
+++ Package.swift
@@ -7,12 +7,11 @@ let package = Package(
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
-    ]
+    ],
     dependencies: [
         
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
-    ],
-
+    ]
 )
 
