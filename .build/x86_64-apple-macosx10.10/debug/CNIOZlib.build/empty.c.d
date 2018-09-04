61d57c4e06fcc569d48b24268848851e45600e57
Hackr <Hello@Camerons-MacBook-Pro.local>
2018-09-03 18:24:52 -0600

    "commit 11.0"

---
+++ .build/x86_64-apple-macosx10.10/debug/CNIOZlib.build/empty.c.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Hello/Desktop/TILApp/.build/checkouts/swift-nio-zlib-support.git--1071467962839356487/module.modulemap \
+  /Users/Hello/Desktop/TILApp/.build/checkouts/swift-nio.git-3108475404973543938/Sources/CNIOZlib/empty.c
