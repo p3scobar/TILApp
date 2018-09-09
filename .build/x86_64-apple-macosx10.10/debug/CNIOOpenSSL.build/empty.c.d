563c7aca835c7909a34e2749b16f4b3958dfe311
Hackr <Hello@Camerons-MacBook-Pro.local>
2018-09-09 11:10:31 -0600

    added gitignore file

--- .build/x86_64-apple-macosx10.10/debug/CNIOOpenSSL.build/empty.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Hello/Desktop/TILApp/.build/checkouts/swift-nio-ssl-support.git--2359138821295600615/module.modulemap \
-  /Users/Hello/Desktop/TILApp/.build/checkouts/swift-nio-ssl.git-1370587408992578247/Sources/CNIOOpenSSL/empty.c
